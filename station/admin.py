356c37c841a16998f61319c4dae803cf2575523a
Anna Lyubinska <anna.lyubinskagmail.com>
2025-10-13 20:38:09 +0200

    feat:Add new models to station app and register them in admin panel

--- station/admin.py
+++ station/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from station.models import TrainType, Train
+from station.models import (TrainType,
+                            Train,
+                            Crew,
+                            Station,
+                            Route,
+                            Journey,
+                            Order,
+                            Ticket)
 
 admin.site.register(TrainType)
 admin.site.register(Train)
+admin.site.register(Crew)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
--- station/models.py
+++ station/models.py
@@ -10,6 +10,7 @@ class TrainType(models.Model):
     def __str__(self):
         return self.name
 
+
 class Train(models.Model):
     name = models.CharField(max_length=100)
     cargo_num = models.IntegerField()
@@ -22,3 +23,88 @@ class Train(models.Model):
     class Meta:
         verbose_name_plural = "trains"
         ordering = ["name"]
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    class Meta:
+        verbose_name_plural = "stations"
+
+    def __str__(self):
+        return f"{self.name} -latitude:{self.latitude}, longitude:{self.longitude}"
+
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Station,
+                               on_delete=models.CASCADE,
+                               related_name="routes_source")
+    destination = models.ForeignKey(Station,
+                                    on_delete=models.CASCADE,
+                                    related_name="routes_destination")
+    distance = models.IntegerField()
+
+    @property
+    def name(self):
+        return f"{self.source.name}-{self.destination.name}"
+
+    class Meta:
+        verbose_name_plural = "routes"
+        ordering = ("source", "destination", "distance")
+
+    def __str__(self):
+        return f"{self.name}, distance: {self.distance} km"
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name="journeys")
+    train = models.ForeignKey(Train, on_delete=models.CASCADE, related_name="journeys")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew)
+
+    def __str__(self):
+        return f"{self.route.name} {self.train.name}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.created_at}"
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(Journey,
+                                on_delete=models.CASCADE,
+                                related_name="tickets")
+    order = models.ForeignKey(Order,
+                              on_delete=models.CASCADE,
+                              related_name="tickets")
+
+    class Meta:
+        unique_together = ("journey", "cargo", "seat")
+        ordering = ["cargo", "seat"]
+
+    def __str__(self):
+        return f"{str(self.journey)} (cargo:{self.cargo}), (seat:{self.seat})"
