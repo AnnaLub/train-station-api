7e5b91c233687281cdd05851759bd3ba836ad362
Anna Lyubinska <anna.lyubinskagmail.com>
2025-11-01 18:55:57 +0100

    feat:Set Authorization by Email instead Username for user

--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {
             "password": {"min_length": 5, "write_only": True},
@@ -20,5 +20,4 @@ class UserSerializer(serializers.ModelSerializer):
         if password:
             user.set_password(password)
             user.save()
-
-
+        return user
