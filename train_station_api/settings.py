9f06e203e444f1091cefe9b79405a930be434058
Anna Lyubinska <anna.lyubinskagmail.com>
2025-12-01 17:50:20 +0100

    chore: add MEDIA_ROO, MEDIA_URL,STATIC_ROOT, STATIC_URL to settings

--- train_station_api/settings.py
+++ train_station_api/settings.py
@@ -136,10 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "/files/static"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/files/media"
 
 MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
